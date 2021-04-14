b894a35f002845fdd9d0c375355fdcee66d3eb8a
benny <benny08118@gmail.com>
2021-04-14 20:36:56 +0800

    index article and show article

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,7 +7,8 @@
 
    <ul>
      @foreach($articles as $article)
-     <li>{{$article->title}}</li>
+     <li><a href="{{route('articles.show',['article'=>$article->id])}}">{{$article->title}}</a></li>
+     
      @endforeach
    </ul>
 
